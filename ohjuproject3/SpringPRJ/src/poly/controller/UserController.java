3209754c2200734070379a021c24c615b3278811
ohju96 <ohju96@naver.com>
2021-08-22 21:25:07 +0900

    dd

--- ohjuproject3/SpringPRJ/src/poly/controller/UserController.java
+++ ohjuproject3/SpringPRJ/src/poly/controller/UserController.java
@@ -4,6 +4,8 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// 카카오 로그인 해보기
+
 @Controller
 public class UserController {
 	
