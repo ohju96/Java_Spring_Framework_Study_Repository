8312fc529f24adfae7a6471b52c09220d2d35c04
ohju96 <ohju96@naver.com>
2021-08-28 20:03:36 +0900

    업데이트

---
+++ study01/SpringProject/src/poly/controller/UserController.java
@@ -0,0 +1,10 @@
+package poly.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+
+@Controller //스프링 프레임워크에게 컨트롤러로 사용할 것임을 알려준다.
+public class UserController {
+
+	private Logger log = Logger.getLogger(this.getClass());
+}
