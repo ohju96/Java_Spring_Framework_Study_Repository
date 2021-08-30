0a858aea0278bbd180db9a0bff0640b2b35a0243
ohju96 <ohju96@naver.com>
2021-08-30 20:26:12 +0900

    간단한 주석 추가

--- study01/SpringProject/src/poly/persistance/mapper/IUserMapper.java
+++ study01/SpringProject/src/poly/persistance/mapper/IUserMapper.java
@@ -2,7 +2,8 @@ package poly.persistance.mapper;
 
 import config.Mapper;
 
-@Mapper("UserMapper")
+// 이 페이지를 맵퍼로 사용할 것임을 동시에 이 페이지 이름을 UserMapper로 지정했음을 알려준다.
+@Mapper("UserMapper") 
 public interface IUserMapper {
 
 }
--- study01/SpringProject/src/poly/service/impl/UserService.java
+++ study01/SpringProject/src/poly/service/impl/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import poly.service.IUserService;
 
+// 이 클래스가 서비스로 사용 될 것임을 알려주는 것과 동시에 이 클래스의 서비스 별칭을 UserService로 정해줬다.
 @Service("UserService")
 public class UserService implements IUserService {
 
