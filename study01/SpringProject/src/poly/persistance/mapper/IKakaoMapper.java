5aef924180cb2d273e8525b9c92234c6b7c470bc
ohju96 <ohju96@naver.com>
2021-08-31 20:14:36 +0900

    카카오 로그인 및 푸쉬 연습

---
+++ study01/SpringProject/src/poly/persistance/mapper/IKakaoMapper.java
@@ -0,0 +1,8 @@
+package poly.persistance.mapper;
+
+import config.Mapper;
+
+@Mapper("KakaoMapper")
+public interface IKakaoMapper {
+
+}
---
+++ study01/SpringProject/src/poly/service/impl/KakaoService.java
@@ -0,0 +1,10 @@
+package poly.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import poly.service.IKakaoService;
+
+@Service("KakaoService")
+public class KakaoService implements IKakaoService {
+
+}
